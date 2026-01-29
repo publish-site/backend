ec24349a05221c0d9677247d95387b2d3449b217
a-rvid <git@rvid.eu>
2026-01-29 09:53:30 +0100

    Implement multithreading with workers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use backend::ThreadPool;
 use std::{
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
@@ -7,11 +8,12 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
@@ -20,14 +22,11 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line: String = buf_reader.lines().next().unwrap().unwrap();
-    println!("{}", request_line);
 
     let (status_line, contents) = if request_line.starts_with("POST ") {
         ("HTTP/1.1 200 OK", "OK")
     } else if request_line.starts_with("SLEEP ") {
-        println!("Sleeping");
         thread::sleep(Duration::from_secs(5));
-        println!("done");
         ("HTTP/1.1 200 OK", "SLEEPED")
     } else {
         ("HTTP/1.1 405 METHOD NOT ALLOWED", "405 METHOD NOT ALLOWED")
