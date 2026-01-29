58fca5701a62d38c4db17b27f0d53f4249d48fc3
a-rvid <git@rvid.eu>
2026-01-29 09:10:57 +0100

    Multithreading with no limits and no worker processes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::{
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration
 };
 
 fn main() {
@@ -9,7 +11,9 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -20,6 +24,11 @@ fn handle_connection(mut stream: TcpStream) {
 
     let (status_line, contents) = if request_line.starts_with("POST ") {
         ("HTTP/1.1 200 OK", "OK")
+    } else if request_line.starts_with("SLEEP ") {
+        println!("Sleeping");
+        thread::sleep(Duration::from_secs(5));
+        println!("done");
+        ("HTTP/1.1 200 OK", "SLEEPED")
     } else {
         ("HTTP/1.1 405 METHOD NOT ALLOWED", "405 METHOD NOT ALLOWED")
     };
