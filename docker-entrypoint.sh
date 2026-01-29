c710ace8d9cc2ab355fe035c0d35eb8d152437d1
a-rvid <git@rvid.eu>
2026-01-29 19:34:47 +0100

    Docker stuff

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-cat <<'EOF' > /etc/nginx/conf.d/deployment-proxy.conf
-server {
-    listen 80;
-    server_name localhost.rvid.eu;
+if [ ! -z PORT ]; then export PORT=80; fi
+if [ -z API_URL ]; then
+  echo "You must specify an API_URL environment variable!"
+  exit 1
+fi
+
+conf="server {
+    listen ${PORT};
+    server_name ${API_URL};
 
     location / {
         proxy_pass http://127.0.0.1:7878;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
     }
-}
-EOF
+}"
+echo "$conf" > /etc/nginx/conf.d/deployment-proxy.conf
 
 nginx -g "daemon off;" & backend
